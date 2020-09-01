e65ce363c9b588084a607b1dd38d795b4d7e62b5
Joshua Ray <joshua.ray1@snhu.edu>
2020-09-01 13:59:59 -0400

    Initial Commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition.Convention;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdleBotWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
