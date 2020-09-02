028d5545f9b9109f3169198d777e8e4e7902ee5d
Joshua Ray <joshua.ray1@snhu.edu>
2020-09-02 08:16:06 -0400

    Created a database service that gathers the database connection string from environment variables

---
+++ Services/DatabaseService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace IdleBotWeb.Services
+{
+    public class DatabaseService
+    {
+        private string ConnectionString { get; set; }
+
+        public DatabaseService(IConfiguration config)
+        {
+            ConnectionString =
+                $"server={config["Server"]};" +
+                $"user={config["User"]};" +
+                $"password={config["Password"]};" +
+                $"database={config["Name"]};" +
+                $"port={config["Port"]}";
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdleBotWeb.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace IdleBotWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton(new DatabaseService(Configuration.GetSection("Database")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
