846e0af6d7b72fa9298569e37cf9a2f70680d946
Joshua Ray <joshua.ray1@snhu.edu>
2020-09-02 16:57:09 -0400

    Created a player model

---
+++ Models/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdleBotWeb.Models
+{
+    public class Player
+    {
+        public ulong Id { get; set; }
+        public string Avatar { get; set; }
+        public string Name { get; set; }
+        public string Faction { get; set; }
+        public string Class { get; set; }
+        public uint CurrentHp { get; set; }
+        public uint Money { get; set; }
+        public uint Level { get; set; }
+        public uint Experience { get; set; }
+        public ushort HealthStat { get; set; }
+        public ushort StrengthStat { get; set; }
+        public ushort DefenseStat { get; set; }
+
+    }
+}
