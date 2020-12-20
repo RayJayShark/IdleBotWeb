242da1c97da377c791ffe7bf87d90a3b563015ba
Joshua Ray <superdog1695@yahoo.com>
2020-12-20 14:16:54 -0500

    Money is now updated with purchase

--- wwwroot/js/shop.js
+++ wwwroot/js/shop.js
@@ -7,6 +7,8 @@
 function Buy(playerId, itemId, itemCost) {
     axios.post(`/Game/Shop/${playerId}?itemId=${itemId}&itemCost=${itemCost}`)
         .then(res => {
+            const money = document.getElementById("money");
+            money.innerText -= itemCost;
             const button = document.getElementById(itemId);
             button.innerText = "Buy";
             button.setAttribute("onclick", `Confirm('${playerId}', ${itemId}, ${itemCost})`);
