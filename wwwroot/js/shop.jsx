bf54646f46ec0944bd403892f5fe314bd1640ffd
Joshua Ray <superdog1695@yahoo.com>
2020-12-22 22:26:32 -0500

    Buttons in shop will now reset to before confirmation when different button is clicked during confirmation

--- wwwroot/js/shop.jsx
+++ wwwroot/js/shop.jsx
@@ -1,32 +1,45 @@
-﻿function Confirm(itemId, itemCost)
-{
+﻿var itemIdToBuy = null;
+var itemCostToBuy = null;
+
+function Confirm(itemId, itemCost) {
+    console.log("Confirm");
     const money = document.getElementById("money");
     if (itemCost > money.innerText) {
         ReactDOM.render(<Error message={"You lack sufficient funds"}/>, document.getElementById('topNotification'));
         return;
     }
+
+    if (itemIdToBuy !== null) {
+        const oldButton = document.getElementById(itemIdToBuy);
+        oldButton.innerText = "Buy";
+        oldButton.setAttribute("onclick", `Confirm(${itemIdToBuy}, ${itemCostToBuy})`);
+    }
     const button = document.getElementById(itemId);
     button.innerText = "Sure?";
-    button.setAttribute("onclick", `Buy(${itemId}, ${itemCost})`);
+    button.setAttribute("onclick", `Buy()`);
+    itemIdToBuy = itemId;
+    itemCostToBuy = itemCost;
 }
 
-function Buy(itemId, itemCost)
-{
+function Buy() {
+    console.log("Buy");
     axios.post(`/Game/Shop/`,
             {
-                Id: itemId,
-                Cost: itemCost
+                Id: itemIdToBuy,
+                Cost: itemCostToBuy
             })
         .then(res =>
         {
             if (res.status === 201)
             {
                 const money = document.getElementById("money");
-                money.innerText -= itemCost;
+                money.innerText -= itemCostToBuy;
             }
-            const button = document.getElementById(itemId);
+            const button = document.getElementById(itemIdToBuy);
             button.innerText = "Buy";
-            button.setAttribute("onclick", `Confirm(${itemId}, ${itemCost})`);
+            button.setAttribute("onclick", `Confirm(${itemIdToBuy}, ${itemCostToBuy})`);
+            itemIdToBuy = null;
+            itemCostToBuy = null;
         });
     ;
 }
